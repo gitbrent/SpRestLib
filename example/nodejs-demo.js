d8877cd47cfb37e6f1e639dbe2fc96a62d05c786
GitBrent <brentely@Brents-Air.home>
2017-05-10 23:06:48 -0500

    Moved work to other branch

--- example/nodejs-demo.js
+++ example/nodejs-demo.js
@@ -10,44 +10,23 @@ console.log(`Starting demo...`);
 
 // SETUP: Load sprestlib and show version to verify everything loaded correctly
 // ============================================================================
-
+var https = require('https');
 var sprLib = require('../dist/sprestlib.js'); // for LOCAL TESTING
 //var sprLib = require("sprestlib");
-console.log(` * sprestlib version: ${sprLib.version()}`); // Loaded okay?
+console.log(` * sprestlib version: ${sprLib.version()}\n`); // Loaded okay?
 
 sprLib.baseUrl('https://gitbrent.sharepoint.com/sites/dev/');
 
-// Node doesnt run in an authenticated webpage, so auth is needed...
-// https://msdn.microsoft.com/en-us/library/dn762763#AccessTokens
-
-var httpntlm = require('httpntlm');
-var credentials = {
-	username: '****@****.onmicrosoft.com',
-	password: '******',
-	domain: '****'
-};
-var webUrl = "https://****.sharepoint.com";
-var fileUrl = "/sites/dev/Shared Documents/AnimGif.pptx";
-
-httpntlm.get({
-    url: "https://****.sharepoint.com/sites/dev/_api/web/sitegroups",
-    username: credentials.username,
-    password: credentials.password,
-    domain: credentials.domain
-}, function (err, res) {
-	if (err) console.log(err);
-	console.log(res.body); //print data
-});
 
 
 
-
-
+/*
 // USER:
 // ============================================================================
 sprLib.user().info()
 .then(function(objUser){ console.table(objUser) })
 .catch(function(errStr){ console.error(errStr)  });
+*/
 
 // ============================================================================
 console.log(`done!`);
