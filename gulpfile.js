f9729814ec26d2edc0688b9b3c99a568993636fe
GitBrent <brentely@Brents-Air.home>
2017-06-23 23:08:28 -0500

    Add new file

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp       = require('gulp'),
+    concat     = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    ignore     = require('gulp-ignore'),
+    uglify     = require('gulp-uglify');
+
+gulp.task('build', function () {
+    gulp.src(['lib/*.js', 'dist/sprestlib.js'])
+        .pipe(concat('sprestlib.bundle.js'))
+        .pipe(sourcemaps.init())
+        .pipe(ignore.exclude(["**/*.map"]))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/'));
+});
