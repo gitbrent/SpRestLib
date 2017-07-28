2f029a7165309e9c3e0c366519d8e2e0d52dcbad
GitBrent <brentely@Brents-Air.home>
2017-07-27 22:41:24 -0500

    Node connectivity success!

--- example/nodejs-demo.js
+++ example/nodejs-demo.js
@@ -1,33 +1,39 @@
 /*
  * NAME: nodejs-demo.js
  * AUTH: Brent Ely (https://github.com/gitbrent/)
- * DATE: May 10, 2017
+ * DATE: Jul 27, 2017
  * DESC: Demonstrate SpRestLib on Node.js
  * REQS: Node 4.x + `npm install sprestlib`
- * EXEC: `node nodejs-demo.js`
+ * EXEC: `node nodejs-demo.js (sp-username) (sp-password) {sp-hostUrl}`
  * REFS: HOWTO: Authenticate to SharePoint Online (*.sharepoint.com)
  * - https://allthatjs.com/2012/03/28/remote-authentication-in-sharepoint-online/
  * - http://paulryan.com.au/2014/spo-remote-authentication-rest/
  * - https://github.com/s-KaiNet/node-spoauth
- */
-console.log(`Starting demo...`);
+*/
+
+// Required Args
+// =============
+if (process.argv.length < 4) {
+	console.log("Usage..: node nodejs-demo.js [spUsername] [spPassword] [spHostUrl]");
+	console.log("Example: node nodejs-demo.js admin@billg.onmicrosoft.com c@ashm0ney https://billg.sharepoint.com");
+	process.exit(-1);
+}
 
 // SETUP: Load sprestlib and show version to verify everything loaded correctly
 // ============================================================================
 var https = require('https'); // this Library is the basis for the remote auth solution
 var sprLib = require('../dist/sprestlib.js'); // for LOCAL TESTING
 //var sprLib = require("sprestlib");
+
+// Lets go
+console.log(`Starting demo...`);
 console.log(` * sprestlib version: ${sprLib.version()}\n`); // Loaded okay?
 
-// TODO: here for testing SpRestLib calls at the end
-//sprLib.baseUrl('https://MYNAME.sharepoint.com/sites/dev/');
-
-
 // Office365 SETUP
-var O365_BASE = "MYNAME"; // TODO:
-var O365_PASS = "MYPASS"; // TODO:
-var O365_HOST = "https://"+O365_BASE+".sharepoint.com/";
-var O365_USER = "admin@"+O365_BASE+".onmicrosoft.com";
+var SP_USER = process.argv[2];
+var SP_PASS = process.argv[3];
+var SP_URL  = process.argv[4];
+var SP_HOST = SP_URL.toLowerCase().replace('https://','').replace('http://','');
 var gBinarySecurityToken = "";
 var gAuthCookie1 = "";
 var gAuthCookie2 = "";
@@ -35,6 +41,8 @@ var gAuthCookie2 = "";
 Promise.resolve()
 .then(function(){
 	// STEP 1: Login to MS with user/pass and get SecurityToken
+	console.log(' * STEP 1/2: Auth into login.microsoftonline.com ...');
+
 	return new Promise(function(resolve,reject) {
 		var xmlRequest = '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\n'
 			+ '  <s:Header>'
@@ -43,15 +51,15 @@ Promise.resolve()
 			+ '    <a:To s:mustUnderstand="1">https://login.microsoftonline.com/extSTS.srf</a:To>'
 			+ '    <o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">'
 			+ '      <o:UsernameToken>'
-			+ '        <o:Username>'+ O365_USER +'</o:Username>'
-			+ '        <o:Password>'+ O365_PASS +'</o:Password>'
+			+ '        <o:Username>'+ SP_USER +'</o:Username>'
+			+ '        <o:Password>'+ SP_PASS +'</o:Password>'
 			+ '      </o:UsernameToken>'
 			+ '    </o:Security>'
 			+ '  </s:Header>'
 			+ '  <s:Body>'
 			+ '    <t:RequestSecurityToken xmlns:t="http://schemas.xmlsoap.org/ws/2005/02/trust">'
 			+ '      <wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy">'
-			+ '        <a:EndpointReference><a:Address>'+ O365_HOST +'</a:Address></a:EndpointReference>'
+			+ '        <a:EndpointReference><a:Address>'+ SP_URL +'</a:Address></a:EndpointReference>'
 			+ '      </wsp:AppliesTo>'
 			+ '      <t:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</t:KeyType>'
 			+ '      <t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType>'
@@ -59,6 +67,7 @@ Promise.resolve()
 			+ '    </t:RequestSecurityToken>'
 			+ '  </s:Body>'
 			+ '</s:Envelope>';
+
 		var options = {
 			hostname: 'login.microsoftonline.com',
 			path    : "/extSTS.srf",
@@ -77,8 +86,13 @@ Promise.resolve()
 				var DOMParser = require('xmldom').DOMParser;
 				var doc = new DOMParser().parseFromString(rawData, "text/xml");
 				// KEY 1: Get SecurityToken
-				gBinarySecurityToken = doc.documentElement.getElementsByTagName('wsse:BinarySecurityToken').item(0).firstChild.nodeValue;
-				resolve();
+				if ( doc.documentElement.getElementsByTagName('wsse:BinarySecurityToken').item(0) ) {
+					gBinarySecurityToken = doc.documentElement.getElementsByTagName('wsse:BinarySecurityToken').item(0).firstChild.nodeValue;
+					resolve();
+				}
+				else {
+					reject('Invalid Username/Password');
+				}
 			});
 		});
 		request.on('error', (e) => {
@@ -91,16 +105,18 @@ Promise.resolve()
 })
 .then(function(){
 	// STEP 2: Provide SecurityToken to SP site and get 2 Auth Cookies
+	console.log(' * STEP 2/2: Auth into SharePoint ...');
+
 	return new Promise(function(resolve,reject) {
 		var options = {
-			hostname: O365_BASE+'.sharepoint.com',
+			hostname: SP_HOST,
 			agent: false,
 			path: "/_forms/default.aspx?wa=wsignin1.0",
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 				'Content-Length': gBinarySecurityToken.length,
-				'Host': O365_BASE+'.sharepoint.com'
+				'Host': SP_HOST
 			}
 		};
 
@@ -121,71 +137,35 @@ Promise.resolve()
 })
 .then(function(data){
 	// STEP 3: Send requests including authentication cookies
-	// EX: `Cookie: FedAuth=77u/PD....LzwvU1A+; rtFA=0U1zw+TnL......AAAA==`
-	return new Promise(function(resolve,reject) {
-		console.log('BAM!!! We authenticated into SharePoint Online!\n');
-		console.log(`gAuthCookie1:\n${gAuthCookie1}\n`);
-		console.log(`gAuthCookie2:\n${gAuthCookie2}\n`);
+	console.log(' * SUCCESS!! Authenticated into "'+ SP_HOST +'"');
+	//console.log(`...gAuthCookie1:\n${gAuthCookie1}\n`);
+	//console.log(`...gAuthCookie2:\n${gAuthCookie2}\n`);
 
-		// TEST: Using Node.js `https` package (works great!!!)
-		var options = {
-			host: O365_BASE+'.sharepoint.com',
-			path: "/sites/dev/_api/web/lists/getbytitle('Employees')?$select=Fields&$expand=Fields",
-			method: 'GET',
-			headers: {
-				"Accept": "application/json;odata=verbose",
-				"Cookie": gAuthCookie1+' ;'+gAuthCookie2,
-			}
-		};
-		var request = https.request(options, (res) => {
-			let rawData = '';
-			res.setEncoding('utf8');
-			res.on('data', (chunk) => rawData += chunk);
-			res.on('end', () => {
-				console.log('TEST: using `https` results:\n');
-				console.log('============================\n');
-				console.log( rawData.substring(0,100)+'...' );
-			});
-		});
-		request.end();
+	// A: SpRestLib requires 2 things: auth-cookie & server-name
+	sprLib.nodeConfig({ cookie:gAuthCookie1+' ;'+gAuthCookie2, server:SP_HOST });
 
+	// B: SpRestLib also needs the full path to your site
+	sprLib.baseUrl('/sites/dev/');
 
-		// TEST BELOW: ----------------------------- doesnt work! ("unauthorized" from SP)
-		// CAUSE: jQuery $.ajax() wont send the cookies!!! It works great when using `https.request` calls!!!!!!
-		/*
-		sprLib.list('Employees').cols()
-		.then(function(data){
-			console.log('DONE!');
-			console.log(data);
-			resolve();
-		})
-		.catch(function(jqXHR,textStatus,errorThrown){
-			console.log('.....nodejs test FAIL:');
-			reject({ 'jqXHR':jqXHR, 'textStatus':textStatus, 'errorThrown':errorThrown });
-		});
-		*/
-	});
+	// C: Now run all the sprLib API calls you want
+	return sprLib.user().info();
+	//return sprLib.list('Employees').cols();
+})
+.then(function(objUser){
+	console.log('sprLib.user().info()');
+	console.log('--------------------');
+	//console.log(objUser);
+	console.log('Id.........: '+ objUser.Id);
+	console.log('Title......: '+ objUser.Title);
+	console.log('LoginName..: '+ objUser.LoginName);
+	console.log('Email......: '+ objUser.Email);
+  })
+.then(function(){
+	console.log(' ');
+	console.log('... Node Demo Complete.');
 })
 .catch(function(strErr){
 	console.error('\n!!! ERROR !!!');
 	console.error(strErr);
 	return;
 });
-
-
-
-
-
-
-
-/*
-// USER:
-// ============================================================================
-sprLib.user().info()
-.then(function(objUser){ console.table(objUser) })
-.catch(function(errStr){ console.error(errStr)  });
-*/
-
-// ============================================================================
-console.log(`done!`);
-// ============================================================================
