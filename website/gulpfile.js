bffc1a3dc77d08caa15b5751691078d264dbf2c2
GitBrent <brentely@Brents-Air.home>
2018-02-06 23:35:10 -0600

    New gulp index.html build process

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,25 +1,39 @@
-var fs      = require('fs'),
-	gulp    = require('gulp'),
-    concat  = require('gulp-concat'),
-    replace = require('gulp-string-replace'),
-    uglify  = require('gulp-uglify');
+/*
+ * DESC: Combine/Minify CSS and take existing min JavaScript - replace head tags with content
+ * WHY.: Google PageSpeed Insights scores of 94/99 thats why!!
+ */
+var fs       = require('fs'),
+    gulp     = require('gulp'),
+    concat   = require('gulp-concat'),
+    replace  = require('gulp-string-replace'),
+	cleanCSS = require('gulp-clean-css');
 
-var cssSrch1 = /\<link rel=\"stylesheet\" href=\"\/SpRestLib\/css\/main\.css\"\/\>/;
+//var cssSrch1 = /\<link rel=\"stylesheet\" href=\"\/SpRestLib\/css\/main\.css\"\/\>/;
+var cssSrch1 = '<link rel="stylesheet" href="/SpRestLib/css/main.css"/>';
 var cssSrch2 = '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/hybrid.min.css"/>';
-// <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+var jvsSrch1 = /\<script type="text\/javascript" src="https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/highlight.*.min.js"\>\<\/script\>/;
 
-gulp.task('default', function(){
-	// STEP 1:
-	gulp.src(['../css/hybrid.min.css', './build/SpRestLib/css/main.css'])
-	.pipe(concat('style.bundle.css'))
-	.pipe(gulp.dest('../css'));
+gulp.task('min-css', function(){
+	// STEP 1: Inline both css files
+	return gulp.src(['../css/hybrid.min.css', './build/SpRestLib/css/main.css'])
+		.pipe(concat('style.bundle.css'))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('../css'))
+		.pipe(gulp.src('../css/style.bundle.css'));
+});
 
-	// STEP 2:
-	var contents = fs.readFileSync('../css/style.bundle.css', 'utf8');
+gulp.task('default', ['min-css'], ()=>{
+	// STEP 2: Grab newly combined styles
+	var strMinCss = fs.readFileSync('../css/style.bundle.css', 'utf8');
+	var strMinJvs = fs.readFileSync('../js/highlight.min.js', 'utf8');
+	//console.log('>> `style.bundle.css` lines = '+ strMinCss.split('\n').length);
+	//console.log('>> `highlight.min.js` lines = '+ strMinJvs.split('\n').length);
 
-	// STEP 3:
+	// STEP 3: Replace head tags with inline style/javascript
 	gulp.src('build/SpRestLib/index.html')
-	.pipe(replace(cssSrch1, '\n<style>'+contents+'</style>\n'))
+	.pipe(replace(cssSrch1, '', {logs:{ enabled:true }}))
+	.pipe(replace(cssSrch2, '\n<style>'+ strMinCss +'</style>\n', {logs:{ enabled:false }}))
+	.pipe(replace(jvsSrch1, '<script>'+ strMinJvs +'</script>\n', {logs:{ enabled:false }}))
 	.pipe(concat('index.perf.html'))
 	.pipe(gulp.dest('../'));
 });
