007a7d8d9134464cbdc549c32992457b9900d72f
GitBrent <brentely@Brents-Air.home>
2018-04-16 23:27:38 -0500

    fixed IE11 polyfill

--- dist/sprestlib.js
+++ dist/sprestlib.js
@@ -2208,8 +2208,8 @@ var NODEJS = ( typeof module !== 'undefined' && module.exports && typeof require
 	}
 })();
 
-// Polyfill (IE11):
-if (window.NodeList && !NodeList.prototype.forEach) {
+// IE11 Polyfill
+if (!NODEJS && typeof window !== 'undefined' && window.NodeList && !NodeList.prototype.forEach) {
 	NodeList.prototype.forEach = function(callback, thisArg) {
 		thisArg = thisArg || window;
 		for (var i = 0; i < this.length; i++){ callback.call(thisArg, this[i], i, this); }
