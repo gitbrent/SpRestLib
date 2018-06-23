7373557dbdcaf86255186a710ba75a074a8f2123
ombayev <ombayev@gmail.com>
2018-06-23 11:15:33 +0600

    rest and list methode should all return Promise

--- dist/sprestlib.d.ts
+++ dist/sprestlib.d.ts
@@ -4,56 +4,56 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace sprLib {
-	const version: string;
+  const version: string;
 
-	function baseUrl(): string;
-	function baseUrl(baseUrl: string): void;
+  function baseUrl(): string;
+  function baseUrl(baseUrl: string): void;
 
-	function nodeConfig(options: object): void;
+  function nodeConfig(options: object): void;
 
-	function renewSecurityToken(): void;
+  function renewSecurityToken(): void;
 
-	/**
-	* SharePoint List/Library API.
-	*
-	* @see \`{@link https://gitbrent.github.io/SpRestLib/docs/api-list.html }\`
-	* @since 1.0
-	*/
-	class list {
-		constructor(listName: string);
-		constructor(listGuid: string);
-		constructor(options: object);
+  /**
+   * SharePoint List/Library API.
+   *
+   * @see \`{@link https://gitbrent.github.io/SpRestLib/docs/api-list.html }\`
+   * @since 1.0
+   */
+  class list {
+    constructor(listName: string);
+    constructor(listGuid: string);
+    constructor(options: object);
 
-		cols(): object[];
-		info(): object[];
-		perms(): object[];
+    cols(): object[];
+    info(): object[];
+    perms(): object[];
 
-		items(options: object): object[];
-		create(options: object): object[];
-		update(options: object): object[];
-		delete(options: object): number;
-		recycle(options: object): number;
-	}
+    items(options: object): Promise<object[]>;
+    create(options: object): Promise<object[]>;
+    update(options: object): Promise<object[]>;
+    delete(options: object): Promise<number>;
+    recycle(options: object): Promise<number>;
+  }
 
-	function rest(options: object): object[];
+  function rest(options: object): Promise<object[]>;
 
-	class site {
-		constructor(siteUrl?: string);
+  class site {
+    constructor(siteUrl?: string);
 
-		info(): object[];
-		lists(): object[];
-		subsites(): object[];
-		perms(): object[];
-		roles(): object[];
-		groups(): object[];
-		users(): object[];
-	}
+    info(): object[];
+    lists(): object[];
+    subsites(): object[];
+    perms(): object[];
+    roles(): object[];
+    groups(): object[];
+    users(): object[];
+  }
 
-	class user {
-		constructor(options: object);
+  class user {
+    constructor(options?: object);
 
-		info(): object;
-		groups(): object[];
-		profile(arrProfileKeys: object): object;
-	}
+    info(): Promise<object>;
+    groups(): object[];
+    profile(arrProfileKeys: object): object;
+  }
 }
